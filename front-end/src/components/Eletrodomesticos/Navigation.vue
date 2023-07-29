3be03cfa8216d4dcdb877b83fa60bc6fdf2893d1
yantisousa <yanemanuel20@outlook.com>
2023-07-29 13:59:17 -0300

    deploy

--- front-end/src/components/Eletrodomesticos/Navigation.vue
+++ front-end/src/components/Eletrodomesticos/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-navigation-drawer>
+    <!-- <v-navigation-drawer v-if="drawer">
         <v-card class="mx-auto" max-width="300">
             <v-list>
 
@@ -62,7 +62,76 @@
                 </v-list-item>
             </v-list>
         </v-card>
+    </v-navigation-drawer> -->
+    <v-navigation-drawer v-model="drawer" temporary>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+            <v-list-item>
+                <div class="text-center">
+                    <v-dialog v-model="dialog" width="auto">
+                        <template v-slot:activator="{ props }">
+                            <v-btn color="primary" v-bind="props">
+                                Cadastrar
+                            </v-btn>
+                        </template>
+
+                        <v-card>
+                            <v-card-text>
+                                <form @submit.prevent="salvar()">
+                                    <v-row>
+                                        <v-col class="input">
+                                            <v-text-field v-model="nome" label="Nome:" variant="outlined">
+
+                                            </v-text-field>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row>
+                                        <v-col class="input">
+                                            <label for="Descrição:">Descrição:</label>
+                                            <div id="app" class="mt-3">
+                                                <ckeditor :editor="editor" v-model="editorData" :config="editorConfig">
+                                                </ckeditor>
+                                            </div>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row>
+                                        <v-col class="input">
+                                            <v-text-field v-model="tensao" label="Tensão:" variant="outlined"
+                                                placeholder="Exemplo: 220v">
+
+                                            </v-text-field>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row>
+                                        <v-col class="input">
+                                            <v-select label="Marcas:" v-model="selectMarca" :items="marcas"
+                                                item-title="nome" variant="underlined" item-value="id"></v-select>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row>
+                                        <v-col class="input">
+                                            <v-btn type="submit" color="primary" block
+                                                @click="dialog = false">PUBLICAR</v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-btn color="primary" block @click="dialog = false">Fechar</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-dialog>
+                </div>
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
+        <div class="ms-10 mt-10">
+            <v-btn color="primary" @click.stop="drawer = !drawer">
+                Menu
+            </v-btn>
+        </div>
 </template>
     
 <script>
@@ -88,7 +157,8 @@ export default defineComponent({
             },
             dialog: false,
             marcas: [],
-            selectMarca: null
+            selectMarca: null,
+            drawer: null,
         }
     },
     mounted() {
